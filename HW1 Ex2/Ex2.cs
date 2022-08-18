97ff61b561224cec0f76d56b6705219821849afa
igorkunovski <igork110@mail.ru>
2022-08-18 20:42:28 +0300

    Homework 1 Ex2

---
+++ HW1 Ex2/Ex2.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и 
+// выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Insert number 1 : ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Insert number 2 : ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>y) {
+Console.WriteLine("max = : " +x);
+}
+else if (y>x){
+    Console.WriteLine("max = : " +y);
+}
+else{
+    Console.WriteLine("Number 1 = Number 2");
+}
