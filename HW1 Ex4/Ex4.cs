a0fb8842c8c9f6e7188a22c65780a7234b71faed
igorkunovski <igork110@mail.ru>
2022-08-18 21:13:21 +0300

    Homework 1 Ex4

---
+++ HW1 Ex4/Ex4.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая принимает 
+// на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+        Console.Write("Insert number 1 : ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Insert number 2 : ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Insert number 3 : ");
+        int z = Convert.ToInt32(Console.ReadLine());
+
+        
+        if (x>=y && x>=z) {           
+                Console.WriteLine("-> " +x);
+        }
+        else if (y>=x && y>=z)  { 
+                Console.WriteLine("-> " +y);
+            }
+        
+        else if (z>=x && z>=y)  { 
+                Console.WriteLine("-> " +z);
+            }
+        else{
+           
+            Console.WriteLine("Numbers are equal");
+        }
+    
+
+  
