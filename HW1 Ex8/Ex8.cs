d3da66e99768dd72107f67991603e1d74a4ca927
igorkunovski <igork110@mail.ru>
2022-08-18 21:44:13 +0300

    Homework 1 Ex8

---
+++ HW1 Ex8/Ex8.cs
@@ -0,0 +1,29 @@
+﻿// Задача 8: Напишите программу, которая на вход 
+// принимает число (N), а на выходе показывает все 
+// чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Insert number : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n==0){
+    Console.Write(n);
+}
+
+else if(n>0){
+
+    for(int i =1; i<=n; i++){
+        if(i%2 ==0){
+            Console.Write(i +" ");
+        }
+    }
+}
+else{
+    for(int i =-1; i>=n; i--){
+        if(i%2 ==0){
+            Console.Write(i +" ");
+        }
+    }
+}
